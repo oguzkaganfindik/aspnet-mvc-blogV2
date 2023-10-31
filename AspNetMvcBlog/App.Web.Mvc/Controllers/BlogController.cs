dab242f84e7deddfd2a852012413b8573b27ff66
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-10-31 16:24:01 +0300

    Gorev5Task2 Tamamlandi

---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Search(string query, int page)
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index(int id, int page)
+        {
+            return View();
+        }
+    }
+}
